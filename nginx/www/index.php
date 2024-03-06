5abcd07fdcbf45d156d58debce328a8c1cdeccba
root <davidasir2020@gmail.com>
2024-03-06 21:20:18 +0000

    chore: trying to fix nginx

---
+++ nginx/www/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<title>
+			PHP Boilerplate 8.2
+		</title>
+	</head>
+	<body>
+		<?php echo php_info(); ?>
+	</body>
+
+</html>
